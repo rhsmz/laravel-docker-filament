1ab163e8f43dbbe078c4160d9d55e7d46684e631
rsmz <me@rsmz.me>
2024-12-03 19:01:27 +0900

    Changes: timers and files migration update

--- database/migrations/2024_12_03_083809_create_timers_table.php
+++ database/migrations/2024_12_03_083809_create_timers_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('timers', function (Blueprint $table) {
+            $table->comment('タイマーを管理するテーブル');
             $table->id();
+            $table->uuid('uuid');
+            $table->string('name')->comment('名前');
+            $table->date('date')->comment('起動日時');
+            $table->string('note')->comment('備考');
+            $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade')->comment('公開ファイルID');
             $table->timestamps();
         });
     }
--- database/migrations/2024_12_03_083828_create_files_table.php
+++ database/migrations/2024_12_03_083828_create_files_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('files', function (Blueprint $table) {
+            $table->comment('ファイルを管理するテーブル');
             $table->id();
+            $table->uuid('uuid');
+            $table->string('name')->comment('名前');
+            $table->string('filename')->comment('ファイル名');
+            $table->string('path')->comment('ファイルパス');
+            $table->string('note')->comment('備考');
             $table->timestamps();
         });
     }
